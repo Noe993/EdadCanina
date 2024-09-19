ac786ec2282c0582e43719f8b38f4f2c730cc03c
Noe <noahs.gapna342@gmail.com>
2024-09-18 20:12:34 -0600

    Se le mueve al mainactivity y si agarra

--- app/src/main/java/com/example/edadcanini/MainActivity.java
+++ app/src/main/java/com/example/edadcanini/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.edadcanini;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        EditText edadEdit = findViewById(R.id.edadEdit);
+        TextView resultText = findViewById(R.id.result_text);
+        Button buton = findViewById(R.id.btnCalc);
+
+        buton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String edad = edadEdit.getText().toString();
+                int edadint = Integer.parseInt(edad);
+                int resultado = edadint * 7;
+                String resultadoString = "La edad del perruno es" + resultado;
+                resultText.setText(resultadoString);
+            }
         });
     }
 }
